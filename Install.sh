23bf85b0c0442d294a6c63cc199ad0c36d4737c7
Thomas <broadbentt19@wells-cathedral-school.com>
2014-11-10 19:51:53 +0000

    Create Install.sh

---
+++ Install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Install
+DIRECTORYLOOP = 'https://github.com/ctrlbadger/Ctrlbadger-MBSSE/blob/master/SamLoop.sh'
+DIRECTORYCOMM = 'https://github.com/ctrlbadger/Ctrlbadger-MBSSE/blob/master/ServerCom'
+
+echo "Welcome to MBSEE\nWe will install the loop and server in this folder
+wget $DIRECTORYLOOP
+
+exit
