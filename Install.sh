05ca84b160542daf39377b566a7de09d31aca304
Thomas <broadbentt19@wells-cathedral-school.com>
2014-11-10 21:20:24 +0000

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-# Install
-DIRECTORYLOOP = 'https://github.com/ctrlbadger/Ctrlbadger-MBSSE/blob/master/SamLoop.sh'
-DIRECTORYCOMM = 'https://github.com/ctrlbadger/Ctrlbadger-MBSSE/blob/master/ServerCom'
+install () {
+echo "Welcome to MBSEE\nWe will install the loop and server in this folder"
+wget https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/SamLoop.sh
+echo "Then install the command in the /bin/usr"
+cd /bin/usr
+wget https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/Server
+}
 
-echo "Welcome to MBSEE\nWe will install the loop and server in this folder
-wget $DIRECTORYLOOP
-
+install
+echo "To start the server type 'Server' in the command line followed by the appropriate arguments"
 exit
