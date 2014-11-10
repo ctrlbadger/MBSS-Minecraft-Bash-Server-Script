0f107d492d4ce03b8c82a41379191ab28ab0b0bb
Thomas <broadbentt19@wells-cathedral-school.com>
2014-11-10 21:42:15 +0000

    Added install, delete, reinstall

--- Install.sh
+++ Install.sh
@@ -1,13 +1,34 @@
 #!/bin/sh
 
 install () {
-echo "Welcome to MBSEE\nWe will install the loop and server in this folder"
-wget https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/SamLoop.sh
-echo "Then install the command in the /bin/usr"
-cd /bin/usr
-wget https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/Server
+  echo "Welcome to MBSEE\nWe will install the loop and server in this folder"
+  wget https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/SamLoop.sh
+  echo "Downloading minecraft server 1.8 jar"
+  wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar
+  echo "Then install the command in the /bin/usr"
+  cd /bin/usr
+  wget https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/Server
 }
 
-install
+reinstall () {
+  echo "Deleting files then installing them again"
+  rm SamLoop.sh
+  rm minecraft_server.1.8.jar
+  wget https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/SamLoop.sh
+  wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar
+  cd /bin/usr
+  rm Server
+  wget https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/Server
+}
+echo "Do you want to (I)nstall|(D)elete|(R)einstall"
+read Install
+if [Install == "I"]; then
+  install
+elif [Install == "D"]
+  delete
+else
+  reinstall
+fi
+
 echo "To start the server type 'Server' in the command line followed by the appropriate arguments"
 exit
