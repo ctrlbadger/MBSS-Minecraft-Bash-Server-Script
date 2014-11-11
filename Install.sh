ebbf7ec04a01501f6a4e25d6671e23a097ff8e6a
Thomas <broadbentt19@wells-cathedral-school.com>
2014-11-11 13:23:20 +0000

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 Loop = "https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/SamLoop.sh"
-Com = "https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar"
-
+Comm = "https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar"
 install () {
   echo "Welcome to MBSEE\nWe will install the loop and server in this folder"
   wget -q "$Loop"
   echo "Downloading minecraft server 1.8 jar"
   wget -q "$Com"
 }
-
 reinstall () {
   echo "Deleting files then installing them again"
   rm SamLoop.sh
@@ -16,7 +14,7 @@ reinstall () {
   wget -q "$Loop"
   wget -q "$Com"
 }
-echo ["Do you want to (I)nstall|(R)einstall"
+echo "Do you want to (I)nstall|(R)einstall"
 read Install
 if [Install == "I"]; then
   install
