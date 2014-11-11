53996cce2fd0a025ff22b5358e6f2339660301b1
Thomas <broadbentt19@wells-cathedral-school.com>
2014-11-11 13:16:53 +0000

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -2,38 +2,22 @@
 
 install () {
   echo "Welcome to MBSEE\nWe will install the loop and server in this folder"
-  wget https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/SamLoop.sh
+  wget -q https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/SamLoop.sh
   echo "Downloading minecraft server 1.8 jar"
-  wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar
-  echo "Then install the command in the /bin/usr"
-  cd /bin/usr
-  wget https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/Server
+  wget -q https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar
 }
 
 reinstall () {
   echo "Deleting files then installing them again"
   rm SamLoop.sh
   rm minecraft_server.1.8.jar
-  wget https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/SamLoop.sh
-  wget https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar
-  cd /bin/usr
-  rm Server
-  wget https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/Server
+  wget -q https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/SamLoop.sh
+  wget -q https://s3.amazonaws.com/Minecraft.Download/versions/1.8/minecraft_server.1.8.jar
 }
-
-delete () {
-  echo "Deleting all files"
-  rm SamLoop.sh
-  rm minecraft_server.1.8.jar
-  cd /bin/usr
-  rm Server
-}
-echo "Do you want to (I)nstall|(D)elete|(R)einstall"
+echo ["Do you want to (I)nstall|(D)elete|(R)einstall"
 read Install
 if [Install == "I"]; then
   install
-elif [Install == "D"]
-  delete
 else
   reinstall
 fi
