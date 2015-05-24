63e774b397525ce505b64d6538d0003dea3657c1
Thomas <broadbentt19@wells-cathedral-school.com>
2015-05-24 13:20:48 +0100

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,20 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #Initialising downloads
-GetLoop = "https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/SamLoop.sh"
-GetJar = "https://s3.amazonaws.com/Minecraft.Download/versions/1.8.4/minecraft_server.1.8.4.jar"
-Sure = 1
-function install {  #Install Function: Used to install the script and jar
+GetLoop="https://raw.githubusercontent.com/ctrlbadger/MBSS-Minecraft-Bash-Server-Script/master/SamLoop.sh"
+GetJar="https://s3.amazonaws.com/Minecraft.Download/versions/1.8.4/minecraft_server.1.8.4.jar"
+Sure=1
+install() {  #Install Function: Used to install the script and jar
   printf "Welcome to MBSEE\nWe will install the loop script and minecraft.jar in this folder"
   wget -q "$GetLoop"
   printf "Downloading minecraft server 1.8 jar"
   wget -q "$GetJar"
 }
 #Reinstall Function
-function reinstall {
+reinstall() {
   printf "\nDeleting files then installing them again"
   printf "\nAre you sure you want to delete than reinstall your files? Y/N :"
   read Sure
-  if [ $Sure == "Y" ];
+  if [ "$Sure" == "Y" ];
   then
     rm SamLoop.sh
     rm minecraft_server.1.8.4.jar
@@ -28,7 +28,7 @@ function reinstall {
 
 echo "Do you want to (I)nstall|(R)einstall"
 read Install
-if [ $Install == "I" ]; then
+if [ "$Install" == "I" ]; then
   install
 else
   reinstall
